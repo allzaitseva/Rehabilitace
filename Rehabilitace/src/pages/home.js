014ceacdcc866784a3cdc2ae61677d05434ef3db
Alena Zaitseva <alena@MBP-George.telekom.ip>
2025-11-21 00:23:49 +0100

    Add navigation bar with responsive menu and hero section

---
+++ Rehabilitace/src/pages/home.js
@@ -0,0 +1,8 @@
+export function HomePage() {
+  return `
+    <div class="page home-page">
+      <h1>Vítejte v Rehabilitaci</h1>
+      <p>Profesionální rehabilitační služby pro vaše zdraví.</p>
+    </div>
+  `;
+}
