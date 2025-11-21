c53109a61cf0be82185f2e6a5aeff3739db9881d
Alena Zaitseva <alena@MBP-George.telekom.ip>
2025-11-21 21:21:17 +0100

    html

--- Rehabilitace/src/pages/home.js
+++
@@ -1,8 +0,0 @@
-export function HomePage() {
-  return `
-    <div class="page home-page">
-      <h1>Vítejte v Rehabilitaci</h1>
-      <p>Profesionální rehabilitační služby pro vaše zdraví.</p>
-    </div>
-  `;
-}
